7c48abce611d97971e4511984382fed5b3b7be8d
vanshit <vanshit.muradh@gmail.com>
2023-04-16 12:50:03 +0530

    admin activities api

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
+const cookieParser = require("cookie-parser");
 app.use(express.json());
+app.use(cookieParser());
+const errorMiddleware = require("./middleware/error");
 
 //Routes imports
 const product = require("./routes/productRoute");
@@ -11,5 +13,7 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 
 app.use("/", product);
+
+app.use(errorMiddleware);
 
 module.exports = app
--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,13 +1,13 @@
-const ErrorHandler = require("../utils/errorhander");
+const ErrorHandler = require("../utils/errorhandler");
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
-  // Wrong mongodb ID error
+  // Wrong Mongodb Id error
   if (err.name === "CastError") {
-    const message = `Resource not found Invalid ${err.path}`
-    err = new ErrorHandler(message, 400)
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
   }
 
   // Mongoose duplicate key error
@@ -28,8 +28,8 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
-  res.status(err.statusCode).json([{
+  res.status(err.statusCode).json({
     success: false,
-    message: err.message
-  }])
+    message: err.message,
+  });
 };
